cfa16c8aa7bfff42933fae2e4aa0da85a90cef7f
Zarifzz <mylegendaries75@gmail.com>
2025-11-16 18:05:32 -0800

    finished AssignRoles route

--- proctor_service/client/main.js
+++ proctor_service/client/main.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const config = require('./client_config.json');
 
 
-const API_URL = config.API_URLS.test; // replace with yours
+const API_URL_test = config.API_URLS.test; // replace with yours
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -15,22 +15,61 @@ function createWindow() {
   win.loadFile("index.html");
 }
 
+/* -------------------------------
+   CALL TEST LAMBDA 
+---------------------------------*/
 ipcMain.handle("callLambda", async () => {
   try {
     // Replace with the JWT you got from Cognito login
     const jwtToken = config.CURR_TOKEN;
-    const response = await axios.get(API_URL, {
+    const response = await axios.get(API_URL_test, {
       headers: {
         Authorization: jwtToken
       }
     });
 
+    //TODO: see what the response.data looks like when you get to here 
     return response.data.message; // or just response.data if you want full response
+
+
   } catch (error) {
+    console.log(error);
     console.error(error.response ? error.response.data : error);
     return "Error contacting API";
   }
 });
+
+
+/* -------------------------------
+   CALL ADMIN-CREATE LAMBDA (NEW)
+   This sends: email, role
+---------------------------------*/
+
+const API_URL_AssignRole = config.API_URLS.AssignRole;
+
+ipcMain.handle("createUser", async (event, userData) => {
+  try {
+    const jwtToken = config.CURR_TOKEN;
+
+    const response = await axios.post(
+      API_URL_AssignRole,
+      {
+        email: userData.email,
+        role: userData.role
+      },
+      {
+        headers: { Authorization: jwtToken }
+      }
+    );
+
+    return response.data.message;
+
+  } catch (error) {
+    console.error(error.response ? error.response.data : error);
+    return { error: "Error creating user" };
+  }
+});
+
 
 
 app.whenReady().then(createWindow);
---
+++ proctor_service/client/test.js
@@ -0,0 +1,4 @@
+const config = require('./client_config.json');
+
+console.log(config.ADMIN.ADMIN_USER);
+
