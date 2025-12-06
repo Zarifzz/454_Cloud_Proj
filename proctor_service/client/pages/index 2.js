b6ed588674574ddfcacc09214a3abfd5c20d8967
Noah-Echo <noah.honkis@gmail.com>
2025-12-05 22:58:51 -0800

    Deleted some duplicated files

--- proctor_service/client/pages/index 2.js
+++
@@ -1,6 +0,0 @@
-// pages/index.js
-const { ipcRenderer } = require("electron");
-
-document.getElementById("goAdmin").addEventListener("click", () => {
-  ipcRenderer.send("navigate-admin");
-});
--- proctor_service/client/test 3.js
+++
@@ -1,4 +0,0 @@
-const config = require('./client_config.json');
-
-console.log(config.ADMIN.ADMIN_USER);
-
